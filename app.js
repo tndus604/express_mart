6f647649a161195a5af87c5a9a013f732355bffe
SuYoun Jeon <jeonsu@flip3.engr.oregonstate.edu>
2023-11-30 17:28:49 -0800

    everthing up to date

--- app.js
+++ app.js
@@ -7,18 +7,29 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 9133;                 // Set a port number at the top so it's easy to change in the future
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
-
 // Database
 var db = require('./database/db-connector')
 
+// app.js
 const { engine } = require('express-handlebars');
+const moment = require('moment');
 var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.engine('.hbs', engine({ // Create an instance of the handlebars engine to process templates
+    extname: ".hbs",
+    // helper function to convert time format as "MM/DD/YYYY"
+    helpers: {
+        formatDate: function (dateString) {   
+            return moment(dateString).format('MM/DD/YYYY');
+        }
+    }
+})); // Create an instance of the handlebars engine to process templates
+
+
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 /*
     ROUTES
@@ -105,15 +116,55 @@ app.get('/employees', function(req, res) {
     });
 }); 
 
-app.get('/transactions', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        res.render('transactions')                                                  
-    }); 
+// show transaction
+app.get('/transactions', function(req, res) {
+    // This query joins the Transactions table with Customers and Employees to get emails instead of IDs
+    let query = `
+        SELECT 
+            Transactions.transactionID, 
+            Customers.email AS customerEmail, 
+            Employees.email AS employeeEmail, 
+            Transactions.purchaseDate, 
+            Transactions.totalAmount
+        FROM Transactions
+        LEFT JOIN Customers ON Transactions.customerID = Customers.customerID
+        LEFT JOIN Employees ON Transactions.employeeID = Employees.employeeID;`;
 
+    db.pool.query(query, function(error, rows, fields) {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500); // Internal Server Error
+        } else {
+            // Render the 'transactions' template with the data retrieved from the database
+            res.render('transactions', { data: rows });
+        }
+    });
+});
+
+// show transaction detail
 app.get('/itemsInTransaction', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        res.render('itemsInTransaction')                                                  
-    }); 
+{
+    let query = `
+        SELECT 
+            ItemsInTransaction.itemID, 
+            Transactions.transactionID AS transactionID, 
+            Products.productName AS productName, 
+            ItemsInTransaction.quantity, 
+            ItemsInTransaction.amount
+        FROM ItemsInTransaction
+        LEFT JOIN Transactions ON ItemsInTransaction.transactionID = Transactions.transactionID
+        LEFT JOIN Products ON ItemsInTransaction.productID = Products.productID
+        ;`;
+    db.pool.query(query, function(error, rows, fields) {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500); // Internal Server Error
+        } else {
+                // Render the 'transactions' template with the data retrieved from the database
+            res.render('itemsInTransaction', { data: rows });
+            }
+        });                                               
+}); 
 
 
 /* =============================== POST ===============================*/
@@ -200,6 +251,115 @@ app.post('/add-employee', function(req, res) {
             // If there is no error, redirect to the employees page to show the updated list
             res.redirect('/employees');
         }
+    });
+});
+
+// add transactions
+app.post('/add-transaction', function(req, res) {
+    let data = req.body;
+    
+    console.log("Received transaction data:", data);
+
+    let customerEmail = data['customerEmail'];
+    let employeeEmail = data['employeeEmail'];
+    let purchaseDate = data['purchaseDate'];
+    let totalAmount = parseFloat(data['totalAmount']);
+
+    // Check if the customer email exists in the Customers table
+    db.pool.query('SELECT * FROM Customers WHERE email = ?', [customerEmail], function(customerCheckError, customerCheckResults) {
+        if (customerCheckError) {
+            console.error("Customer check error:", customerCheckError);
+            return res.status(500).send('Error checking customer email');
+        }
+
+        console.log("Customer check results:", customerCheckResults);
+
+        if (customerCheckResults.length === 0) {
+            return res.status(400).send('Customer email not found');
+        }
+
+        let customerID = customerCheckResults[0].customerID;
+
+        // Check if the employee email exists in the Employees table
+        db.pool.query('SELECT * FROM Employees WHERE email = ?', [employeeEmail], function(employeeCheckError, employeeCheckResults) {
+            if (employeeCheckError) {
+                console.error("Employee check error:", employeeCheckError);
+                return res.status(500).send('Error checking employee email');
+            }
+
+            console.log("Employee check results:", employeeCheckResults);
+
+            if (employeeCheckResults.length === 0) {
+                return res.status(400).send('Employee email not found');
+            }
+
+            let employeeID = employeeCheckResults[0].employeeID;
+
+            // Insert data into Transactions
+            let queryTransactionsInsert = `INSERT INTO Transactions (customerID, employeeID, purchaseDate, totalAmount) VALUES (?, ?, ?, ?);`;
+            let queryParams = [customerID, employeeID, purchaseDate, totalAmount];
+
+            db.pool.query(queryTransactionsInsert, queryParams, function(insertError, insertResults) {
+                if (insertError) {
+                    console.error("Insert transaction error:", insertError);
+                    return res.status(400).send('Error adding new transaction');
+                }
+
+                console.log("Transaction inserted successfully");
+                res.redirect('/transactions');
+            });
+        });
+    });
+});
+
+// add transaction details
+app.post('/add-itemsInTransaction', function(req, res) {
+    let data = req.body;
+
+    console.log("Received items in transaction data:", data);
+
+    let transactionID = data['transactionID'];
+    let productName = data['productName'];
+    let quantity = parseInt(data['quantity']);
+    let amount = parseFloat(data['amount']);
+
+    // Check if the transaction ID exists in the Transactions table
+    db.pool.query('SELECT * FROM Transactions WHERE transactionID = ?', [transactionID], function(transactionCheckError, transactionCheckResults) {
+        if (transactionCheckError) {
+            console.error("Transaction check error:", transactionCheckError);
+            return res.status(500).send('Error checking transaction ID');
+        }
+
+        if (transactionCheckResults.length === 0) {
+            return res.status(400).send('Transaction ID not found');
+        }
+
+        // Check if the product ID exists in the Products table
+        db.pool.query('SELECT * FROM Products WHERE productName = ?', [productName], function(productCheckError, productCheckResults) {
+            if (productCheckError) {
+                console.error("Product check error:", productCheckError);
+                return res.status(500).send('Error checking product Name');
+            }
+
+            if (productCheckResults.length === 0) {
+                return res.status(400).send('Product Name not found');
+            }
+            let productID = productCheckResults[0].productID;
+
+            // Insert data into ItemsInTransaction
+            let queryItemsInTransactionInsert = `INSERT INTO ItemsInTransaction (transactionID, productID, quantity, amount) VALUES (?, ?, ?, ?);`;
+            let queryParams = [transactionID, productID, quantity, amount];
+
+            db.pool.query(queryItemsInTransactionInsert, queryParams, function(insertError, insertResults) {
+                if (insertError) {
+                    console.error("Insert items in transaction error:", insertError);
+                    return res.status(400).send('Error adding new item in transaction');
+                }
+
+                console.log("Item in transaction inserted successfully");
+                res.redirect('/itemsInTransaction'); // Adjust the redirect as needed
+            });
+        });
     });
 });
 
@@ -348,6 +508,84 @@ app.post('/update-employee', function(req, res) {
     });
 });
 
+// update transaction
+app.post('/update-transaction', function(req, res) {
+    let data = req.body;
+
+    let transactionID = parseInt(data['updateTransactionID']);
+    let purchaseDate = data['updatePurchaseDate'];
+    let totalAmount = parseFloat(data['updateTotalAmount']);
+
+    if (isNaN(transactionID)) {
+        return res.status(400).send('Invalid transaction ID');
+    }
+
+    let queryUpdateTransaction = `UPDATE Transactions SET purchaseDate = ?, totalAmount = ? WHERE transactionID = ?;`;
+    let queryParams = [purchaseDate, totalAmount, transactionID];
+
+    db.pool.query(queryUpdateTransaction, queryParams, function(updateError, updateResults) {
+        if (updateError) {
+            console.error("Update transaction error:", updateError);
+            return res.status(400).send('Error updating transaction');
+        }
+
+        console.log("Transaction updated successfully");
+        res.redirect('/transactions');
+    });
+});
+
+// update transaction detail
+app.post('/update-transactionDetail', function (req, res, next) {
+    let data = req.body;
+
+    let itemID = parseInt(data.updateItemID);
+    let productName = data.updateProductName; 
+    let quantity = parseInt(data.updateQuantity);
+    let amount = parseFloat(data.updateAmount);
+
+    if (isNaN(itemID)) {
+        return res.status(400).send('Invalid item ID');
+    }
+    
+    console.log("Received request to update item with ID:", itemID);
+    console.log("Product Name to update:", productName);
+    console.log("New Quantity:", quantity);
+    console.log("New Amount:", amount);
+
+    // First, get the productID for the given productName
+    db.pool.query('SELECT productID FROM Products WHERE productName = ?', [productName], function(productCheckError, productCheckResults) {
+        if (productCheckError) {
+            console.error("Error checking product name:", productCheckError);
+            return res.status(500).send('Server error occurred while checking product name.');
+        }
+
+        if (productCheckResults.length === 0) {
+            console.log("Product name not found in the database:", productName);
+            return res.status(400).send('Product name not found.');
+        }
+
+        let productID = productCheckResults[0].productID;
+
+        // Now update the ItemsInTransaction with the obtained productID
+        let queryUpdateItemsInTransaction = `
+            UPDATE ItemsInTransaction 
+            SET productID = ?, quantity = ?, amount = ? 
+            WHERE itemID = ?;
+        `;
+        let queryParams = [productID, quantity, amount, itemID];
+
+        db.pool.query(queryUpdateItemsInTransaction, queryParams, function (updateError, updateResults) {
+            if (updateError) {
+                console.error("Error updating item in transaction:", updateError);
+                return res.status(400).send('Server error occurred while updating item in transaction.');
+            }
+
+            console.log("Item in transaction updated successfully:", itemID);
+            res.redirect('/itemsInTransaction'); // Redirect as appropriate for your app
+        });
+    });
+});
+
 
 /* =============================== DELETE ===============================*/
 
@@ -414,6 +652,40 @@ app.delete('/delete-employee', function(req, res, next){
     db.pool.query(queryDeleteEmployee, [employeeID], function(error, results, fields){
         if (error) {
             // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.sendStatus(204); 
+        }
+    });
+});
+
+// delete transaction
+app.delete('/delete-transaction', function(req, res, next){
+    let data = req.body;
+    let transactionID = parseInt(data.transactionID); 
+    let queryDeleteTransaction = `DELETE FROM Transactions WHERE transactionID = ?`;
+
+    // Run the query
+    db.pool.query(queryDeleteTransaction, [transactionID], function(error, results, fields){
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.sendStatus(204); 
+        }
+    });
+});
+
+// delete transaction detail
+app.delete('/delete-transactionDetail', function(req, res, next){
+    let data = req.body;
+    let itemID = parseInt(data.itemID); 
+    let queryDeleteItemsInTransaction = `DELETE FROM ItemsInTransaction WHERE itemID = ?`;
+
+    // Run the query
+    db.pool.query(queryDeleteItemsInTransaction, [itemID], function(error, results, fields){
+        if (error) {
             console.log(error);
             res.sendStatus(400);
         } else {
