04f81603dca5e80286bb615093b65eb553226c19
SuYoun Jeon <suyoun@suyouns-mbp.home>
2024-07-29 15:10:22 -0400

    Update database connection to use JawsDB

--- database/db-connector.js
+++ database/db-connector.js
@@ -4,17 +4,31 @@
 // Date: Nov, 15th, 2023
 
 require('dotenv').config();
-var mysql = require('mysql')
+var mysql = require('mysql');
+var url = require('url');
 
-// Create a 'connection pool' using the provided credentials
+// Parse the JawsDB connection URL
+var JAWSDB_TEAL_URL = process.env.JAWSDB_TEAL_URL;
+var connectionString = url.parse(JAWSDB_TEAL_URL);
+var auth = connectionString.auth.split(':');
+
+// // Create a 'connection pool' using the provided credentials
+// var pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT, // Include this line to specify the port
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// })
+
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // Include this line to specify the port
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+    host: connectionString.hostname,
+    user: auth[0],
+    password: auth[1],
+    database: connectionString.pathname.substring(1)
+});
 
 // Export it for use in our applicaiton
 module.exports.pool = pool;
