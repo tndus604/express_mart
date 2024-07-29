19d2c178432ecd4fe234afea98449904a0ae3585
SuYoun Jeon <suyoun@suyouns-mbp.home>
2024-07-29 14:56:28 -0400

    Use dynamic port from environment variable

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 46686;                 // Set a port number at the top so it's easy to change in the future
+var PORT = process.env.PORT || 3000;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
 var db = require('./database/db-connector')
